9dc4a4c43c6d5993a998c12361c413e24b0eac57
Welease <tanzilya.farruhshina.01@mail.ru>
2021-06-13 11:36:40 +0300

    add author name to view

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,9 +1,7 @@
 package com.example.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 import com.example.sweater.domain.User;
 
 @Entity
@@ -14,6 +12,9 @@ public class Message {
 
     private String text;
     private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private User author;
 
     public User getAuthor() {
@@ -27,9 +28,14 @@ public class Message {
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User author) {
+        this.author = author;
         this.text = text;
         this.tag = tag;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
     }
 
     public void setText(String text) {
--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.sweater.service;
 
 import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,14 +9,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService {
-    private final UserRepo userRepo;
-
-    public UserService(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
+    @Autowired
+    private UserRepo userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return userRepo.findByUsername(username);
     }
 }
